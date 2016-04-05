47cb504d6b478d4a59b496dd27253151735111f9
Sai Raghu Neelothpal Saraswatibhatla <neelu.rsa@gmail.com>
2016-04-05 11:15:39 +0100

    add features

---
+++ moduletest.py
@@ -0,0 +1,28 @@
+import eightvar
+import sys
+
+eightvar.prnt("""out1
+outln
+outln
+outln
+outln
+dly1
+out2
+dly1
+out3
+dly1
+out4
+dly1
+out5
+dly1
+out6
+dly1
+out7
+dly1
+out8
+dly1
+out9
+dly1
+out10
+dly1
+""")
